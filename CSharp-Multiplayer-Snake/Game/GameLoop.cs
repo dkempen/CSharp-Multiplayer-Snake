63be56bbccdb88344b5a9af11d8170b4c34811e3
dkempen <daan.vankempen2@gmail.com>
2018-09-29 12:48:04 +0200

    [Added] Project and Class structure + Gitignore

---
+++ CSharp-Multiplayer-Snake/Game/GameLoop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Multiplayer_Snake
+{
+    class GameLoop
+    {
+        private List<Snake> snakes;
+
+        public GameLoop(List<Snake> snakes)
+        {
+            this.snakes = snakes;
+
+          //  snakes.CurrentHeadPos(1,1)
+                //Stack -> remove tail pos
+
+        }
+    }
+}
---
+++ SharedSnakeGame/Networking/TcpHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedSnakeGame.Networking
+{
+    class TcpHandler
+    {
+    }
+}
